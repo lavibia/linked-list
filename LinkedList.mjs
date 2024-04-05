ddbae17c252530e9256c5422eb7b93e74f86dbe0
Lavinia Bianca <chelaru_laviniabianca@yahoo.com>
2024-04-06 01:52:32 +0200

    Update tests

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -1,53 +1,101 @@
 import { Node } from './Node.mjs';
 
 export class LinkedList {
-    constructor(head = null, size = 0, tail = null) {
-        this.head = head;
-        this.size = size;
-        this.tail = tail;
-    }
+  constructor(head = null, size = 0, tail = null) {
+    this.head = head;
+    this.size = size;
+    this.tail = tail;
+  }
 
-    append(value) {
-        let newNode = new Node();
-        newNode.value = value;
-        this.tail = newNode;
-        if (this.head == null) {
-            this.head = newNode;
+  append(value) {
+    let newNode = new Node();
+    newNode.value = value;
+    this.tail = newNode;
+    if (this.head == null) {
+      this.head = newNode;
+    }
+    else {
+      let temp = this.head;
+      while (temp.next != null) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
+    }
+    this.size += 1;
+  }
+  prepend(value) {
+    let newNode = new Node();
+    newNode.value = value;
+    let next = this.head;
+    newNode.next = next;
+    this.head = newNode;
+    this.size += 1;
+  }
+  size() {
+    return this.size;
+  }
+  head() {
+    return this.head;
+  }
+  tail() {
+    return this.tail;
+  }
+  at(index) {
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      if (temp == null)
+        return null;
+      temp = temp.next;
+    }
+    return temp;
+  }
+
+  pop() {
+    if (this.head != null) {
+      let temp = this.head;
+      if (temp.next == null) {
+        this.head = null;
+        this.tail = null;
+      }
+      else {
+        while (temp.next.next != null) {
+          temp = temp.next;
         }
-        else {
-            let temp = this.head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-        }
-        this.size += 1;
+        temp.next = null;
+        this.tail = temp;
+      }
     }
-    prepend(value) {
-        let newNode = new Node();
-        newNode.value = value;
-        let next = this.head;
-        newNode.next = next;
-        this.head = newNode;
-        this.size += 1;
+  }
+  contains(value) {
+    let temp = this.head;
+    while (temp != null) {
+      if (temp.value === value) {
+        return true;
+      }
+      temp = temp.next;
     }
-    size() {
-        return this.size;
+    return false;
+  }
+  find(value) {
+    let temp = this.head;
+    let i = 0;
+    while (temp != null) {
+      if (temp.value === value) {
+        return i;
+      }
+      i++;
+      temp = temp.next;
     }
-    head() {
-        return this.head;
+    return false;
+  }
+  toString() {
+    let temp = this.head;
+    let str = '';
+    while (temp != null) {
+      str += `(${temp.value})->`;
+      temp = temp.next;
     }
-    tail() {
-        return this.tail;
-    }
-    at(index) {
-        let temp = this.head;
-        for (let i = 0; i < index; i++) {
-            if (temp == null)
-                return null;
-            temp = temp.next;
-        }
-        return temp;
-    }
+    return str + ' null';
+  }
 }
 
--- test.mjs
+++ test.mjs
@@ -1,7 +1,17 @@
-import{LinkedList} from './LinkedList.mjs';
+import { LinkedList } from './LinkedList.mjs';
 
-let list= new LinkedList();
-list.append('test');
-list.prepend('textFirst');
+let list = new LinkedList();
+list.append('test'); //1
+list.prepend('textFirst'); //0
+list.append('third'); //2
 
-console.log(list, list.size, list.head, list.tail, list.at(200));
+console.log(`Initial list: ${list.toString()} `)
+console.log(`The list size: ${list.size}`);
+console.log(`The list tail: ${list.tail}`);
+console.log(`The list head: ${list.head}`);
+console.log(`Second list Node: ${list.at(1)}`);
+list.pop();
+console.log(`List after the last node is removed: ${list}`); 
+console.log(`Does the list contains the value 'something'? : ${list.contains('something')}`);
+console.log(`Index of 'test'. False if not found: ${list.find('test')}`);
+console.log(`The final list is: ${list.toString()}`);
